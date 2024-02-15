f24b682f9daa03f4aebeb30be36e5f8ea324696d
bwyma1 <brockdwyma@gmail.com>
2024-02-15 15:24:22 -0600

    fixed bug with posting new appointment

--- backend/src/models/Appointment.ts
+++ backend/src/models/Appointment.ts
@@ -6,7 +6,8 @@ class Appointment {
     static APPOINTMENT_TABLE = "appointment";
 
     static findAppoitnmentByTutor(tutor_id: number, start_date: Date){
-        return knex(this.APPOINTMENT_TABLE).select('*').where({tutor_id,start_time:start_date.toISOString()})
+        const start = start_date.toISOString()
+        return knex(this.APPOINTMENT_TABLE).select('*').where({tutor_id,start_time:start_date.getTime()})
     }
 
     static async createAppointment(newAppointment: Omit<IAppointment, "id">) {
