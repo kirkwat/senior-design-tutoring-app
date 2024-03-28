a2bf71717b1f377ca9e189e821b762c5c882711c
thannansmu <thannan@smu.edu>
2024-03-28 13:59:33 -0500

    Finished tutor profile page and edit page

--- backend/src/routes/appointment.ts
+++ backend/src/routes/appointment.ts
@@ -11,17 +11,17 @@ import verifyRoles from "../middleware/verifyRole";
 const router = express.Router();
 
 router
-  // .post("/:tutorID", verifyRoles("tutor"), handleNewAppointment)
-  // .put("/:appointmentID", verifyRoles("user"), handRegisterForAppointment)
-  // .get("/", verifyRoles("user", "tutor"), handleFindAvailableAppointments)
-  // .get("/available", verifyRoles("user", "tutor"), handleIsAvailable)
-  // .get("/student/:studentID", verifyRoles("user"), handleFindStudentsAppointments);
+  .post("/:tutorID", verifyRoles("tutor"), handleNewAppointment)
+  .put("/:appointmentID", verifyRoles("user"), handRegisterForAppointment)
+  .get("/", verifyRoles("user", "tutor"), handleFindAvailableAppointments)
+  .get("/available", verifyRoles("user", "tutor"), handleIsAvailable)
+  .get("/student/:studentID", verifyRoles("user"), handleFindStudentsAppointments);
 
-  .post("/:tutorID", handleNewAppointment)
-  .put("/:appointmentID", handRegisterForAppointment)
-  .get("/", handleFindAvailableAppointments)
-  .get("/available", handleIsAvailable)
-  .get("/student/:studentID", handleFindStudentsAppointments);
+  // .post("/:tutorID", handleNewAppointment)
+  // .put("/:appointmentID", handRegisterForAppointment)
+  // .get("/", handleFindAvailableAppointments)
+  // .get("/available", handleIsAvailable)
+  // .get("/student/:studentID", handleFindStudentsAppointments);
 
 
 
--- backend/src/routes/tutor.ts
+++ backend/src/routes/tutor.ts
@@ -13,19 +13,19 @@ import { handleFindTutorsSubjects } from "../controllers/subjectController";
 const router = express.Router();
 
 router
-  // .get("/available", verifyRoles("user"), handleFindAvailableTutorsByTime)
-  // .get("/available/day", verifyRoles("user"), handleFindAvailableTutorsByDay)
-  // .get("/available/week", verifyRoles("user"), handleFindAvailableTutorsByWeek)
-  // .get("/:tutorID", handleFindTutorByID)
-  // .get("/", verifyRoles("user"), handleFindAllTutors)
-  // .put("/profile/:tutorID", verifyRoles("tutor"), handleUpdateTutorProfile)
-
-  .get("/available", handleFindAvailableTutorsByTime)
-  .get("/available/day", handleFindAvailableTutorsByDay)
-  .get("/available/week", handleFindAvailableTutorsByWeek)
+  .get("/available", verifyRoles("user"), handleFindAvailableTutorsByTime)
+  .get("/available/day", verifyRoles("user"), handleFindAvailableTutorsByDay)
+  .get("/available/week", verifyRoles("user"), handleFindAvailableTutorsByWeek)
   .get("/:tutorID", handleFindTutorByID)
-  .get("/:tutorID/subjects", handleFindTutorsSubjects)
-  .get("/", handleFindAllTutors)
-  .put("/profile/:tutorID", handleUpdateTutorProfile)
+  .get("/", verifyRoles("user"), handleFindAllTutors)
+  .put("/profile/:tutorID", verifyRoles("tutor"), handleUpdateTutorProfile)
+
+  // .get("/available", handleFindAvailableTutorsByTime)
+  // .get("/available/day", handleFindAvailableTutorsByDay)
+  // .get("/available/week", handleFindAvailableTutorsByWeek)
+  // .get("/:tutorID", handleFindTutorByID)
+  // .get("/:tutorID/subjects", handleFindTutorsSubjects)
+  // .get("/", handleFindAllTutors)
+  // .put("/profile/:tutorID", handleUpdateTutorProfile)
 
 export default router;
